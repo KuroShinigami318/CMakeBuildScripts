3ac3439ddea8da16d0231f29a12a7d9d74fb3bc7
bao.duongngoc <duongngocbao318@gmail.com>
2024-05-14 23:10:41 +0700

    update secret_key

--- repo_init.sh
+++ repo_init.sh
@@ -1,7 +1,7 @@
 cd `dirname $0`
 SCRIPTDIR=`pwd`
 
-encryptedapikey=U2FsdGVkX1/MPerBIjp7zLVruoda4faDQswS9h9mC0Rs7FBmcrhJOEsMmkpNoEx1ZBqUIW/oXynROk8Zet2UUCZ527Fh0hRF86i9e2siSnuP+f6efRd3M8x+M6o6hK54Umgi44iSkMEhe008yXTIkQ==
+encryptedapikey=U2FsdGVkX1/KRyOHBhtFg4TGIORRxsPgn2NVXZA/hGFfG7xvFc3jFFKWeenovbikDDKdGlSpdZwZ65JX/9rIhplnskbaROt6lW8z/+rmdCGliPfzSHUJDP77YcUtv5EryV4NG7VA/t3RlJ7eJIfAmg==
 owner=KuroShinigami318
 apikey=$(./decrypt.sh $encryptedapikey)
 
