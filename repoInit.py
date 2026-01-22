4a4346a3eb34f37a99a7eeb19952945677cd491f
KuroShinigami318 <53936920+KuroShinigami318@users.noreply.github.com>
2026-01-23 01:44:23 +0700

    update scripts

--- repoInit.py
+++ repoInit.py
@@ -1,14 +1,16 @@
-from getDeps import ReposManager
+from pathlib import Path
+import subprocess
 
 def execute():
     f = open('credentials', 'r')
     api_key = f.read().strip()
     f.close()
     owner = 'KuroShinigami318'
+    script_path = Path(__file__).resolve()
+    root_path = script_path.parent.parent
     libs_dep_relative_path = 'libs_dep.json'
     dependencies_folder_name = 'libs'
-    reposManager = ReposManager(api_key, owner)
-    reposManager.traverseAllDep(libs_dep_relative_path, dependencies_folder_name)
+    subprocess.run(['python', f'scripts/getDeps.py', api_key, owner, libs_dep_relative_path, dependencies_folder_name], cwd=root_path, check=True)
 
 def main():
     execute()
